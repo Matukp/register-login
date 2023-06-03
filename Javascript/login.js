27e7aa504a93005a9d14296650fd23bd0c0edd80
Matias Marchsese <102271123+Matukp@users.noreply.github.com>
2023-06-02 23:49:56 -0300

    primer commit

---
+++ Javascript/login.js
@@ -0,0 +1,16 @@
+const loginform = document.querySelector('#loginForm');
+loginform.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const validUser = Users.find(user => user.email === email && user.password === password)
+    if(!validUser){
+        return alert('usuario y/o contrasena incorrecto')
+    }
+
+    alert(`Bienvenido ${validUser.name}`)
+    localStorage.setItem('login_success', JSON.stringify(validUser))
+    window.location.href = 'index.html'
+
+})
